3683d7feb94c3a4a986a8fc03826706c0a29eb06
AbdievMarat <abdiev.m.t@gmail.com>
2022-07-31 21:28:28 +0600

    navbar background color edit

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -31,7 +31,8 @@ AppAsset::register($this);
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
+            'class' => 'navbar navbar-expand-md navbar-dark fixed-top',
+            'style' => 'background-color: #9c27b0;'
         ],
     ]);
 
