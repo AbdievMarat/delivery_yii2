8a4b6a7fc5bbcb22a0419df118335a440c2e1266
AbdievMarat <abdiev.m.t@gmail.com>
2022-08-01 10:09:39 +0600

    navbar button logout background color edit

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -75,7 +75,7 @@ AppAsset::register($this);
         echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
             . Html::submitButton(
                 '<i class="bi bi-box-arrow-in-right"></i> Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout text-decoration-none']
+                ['class' => 'btn btn-primary logout']
             )
             . Html::endForm();
     }
